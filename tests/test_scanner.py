b3e615a6feed4390572dee3ad2772695634df2ff
ardasezer <ardasezer.253@gmail.com>
2026-01-20 08:33:34 +0300

    Implement tests for PackageManager functionality
    Add unit tests for PackageManager class

--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -1,1 +1,12 @@
+import unittest
+from src.main import PackageManager
 
+class TestLPMA(unittest.TestCase):
+    def setUp(self):
+        self.tool = PackageManager()
+
+    def test_database_load(self):
+        self.assertTrue(len(self.tool.mock_db) > 0)
+
+if __name__ == '__main__':
+    unittest.main()
